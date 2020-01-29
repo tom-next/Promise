5bb4333d970bc97ca1e8135dba15b3e7d32293f6
tom-next <39457057+tom-next@users.noreply.github.com>
2020-01-29 22:59:49 +0800

    实现promise主要功能

--- index.js
+++ index.js
@@ -1,85 +1,29 @@
-let events = require('events')
-const log = console.log.bind(console)
-
-// 这里先要实现一遍 发布/订阅模式
-// TomEvent 继承了 node 中 events.EventEmitter 因此可以使用 发布/订阅模式
-class TomEvent extends events.EventEmitter{
-    constructor() {
-        super()
-    }
-    // then 是将回调函数存起来，利用发布/订阅机制
-    then(fulfilledHandler, errorHandler, progressHandler) {
-        if(Object.prototype.toString.call(fulfilledHandler) === '[object Function]') {
-            // 函数
-            this.once('success', fulfilledHandler)
-        }
-        if(Object.prototype.toString.call(errorHandler) === '[object Function]') {
-            // 函数
-            this.once('error', errorHandler)
-        }
-        if(Object.prototype.toString.call(progressHandler) === '[object Function]') {
-            // 函数
-            this.once('progress', progressHandler)
-        }
-        return this
-    }
-}
-
-// 触发执行上面回调函数的地方
-// promise内部有三种状态: 未完成态、完成态和失败态。
-class TomPromise {
+// 实现 promiseAs
+class PromiseA {
     constructor(func) {
-        // 接收一个参数
-        this.tomEvents = new TomEvent()
-        this.state = 'unfulfilled'
-        this.finished = false
-        this.resolve = this.resolve.bind(this)
-        this.reject = this.reject.bind(this)
-        this.progress = this.progress.bind(this)
-        if(Object.prototype.toString.call(func) === '[object Function]') {
-            // 函数
-            func(this.resolve, this.reject)
-        }else {
-            // 这里应该做错误处理
+        this.succArg = undefined
+        this.failArg = undefined
+        this.successCallBacks = []
+        this.failCallBacks = []
+        this.STATUS = {
+            PENDING: 1,
+            RESOLVE: 2,
+            REJECT: 3,
         }
+        this._status = this.STATUS.PENDING
+        this._execFun(func)
     }
 
-    then(fulfilledHandler, errorHandler, progressHandler) {
-        return this.tomEvents.then(fulfilledHandler, errorHandler, progressHandler)
-    }
-
-    resolve(obj) {
-        this.state = 'fulfilled'
-        this.tomEvents.emit('success', obj)
-    }
-
-    reject(obj) {
-        // 这里应该做一个判断,只要有 reject, 当前的状态为 failed, 其他也立即停止
-        if(this.finished) {
-            // 这里应该返回另外的一个逻辑来调用 catch
-            return;
-        }
-        this.finished = true
-        this.state = 'failed'
-        this.tomEvents.emit('error', obj)
-    }
-
-    progress(obj) {
-        this.tomEvents.emit('progress', obj)
-    }
-
-    static all(arr) {
-        // 默认接受 promise 数组
-        let len = arr.length
+    static all(array) {
         let result = []
-        // 这里还是创建一个 promise 来记录当前循环的状态
-        let t = new this((resolve, reject) => {
-            for (let i = 0 ;i < arr.length; i++) {
-                let promise = arr[i]
+        let len = array.length
+        return new this((resolve, reject) => {
+            for (let i = 0; i < array.length; i++) {
+                let promise = array[i]
                 promise.then((data) => {
                     len--
                     result[i] = data
-                    if(len === 0) {
+                    if (len === 0) {
                         resolve(result)
                     }
                 }, (err) => {
@@ -87,55 +31,150 @@ class TomPromise {
                 })
             }
         })
-        return t.then((data) => {
-            console.log("都成功了", data)
-        }, (err) => {
-            console.log("没有成功", err)
+    }
+
+    static race(array) {
+        return new this((resolve, reject) => {
+            for (let i = 0; i < array.length; i++) {
+                let promise = array[i]
+                promise.then((data) => {
+                    resolve(data)
+                    return
+                }, (err) => {
+                    reject(err)
+                })
+            }
         })
     }
 
+    _isFunction(f) {
+        return Object.prototype.toString.call(f) === '[object Function]'
+    }
+
+    _execFun(fun) {
+        let that = this
+        if (this._isFunction(fun)) {
+            fun(function () {
+                // 这里将异步的结果保存在 successArg 中
+                that.succArg = Array.prototype.slice.apply(arguments)
+                that._status = that.STATUS.RESOLVE;
+                // 将参数传下去
+                that.resolve.apply(that, arguments)
+            }, function () {
+                // 失败的结果保存在 failArg 中
+                that.failArg = Array.prototype.slice.apply(arguments)
+                that._status = that.STATUS.REJECT
+
+                that.reject.apply(that, arguments)
+            })
+        } else {
+            this.resolve(fun)
+        }
+    }
+
+    resolve() {
+        // arg里面包括了异步返回的结果
+        let arg = arguments
+        let callback = this.successCallBacks.shift();
+        if (this._status === this.STATUS.RESOLVE && callback) {
+            // 执行回调函数 callback, 并且将执行结果给回调函数
+            callback.apply(callback, arg)
+        }
+    }
+
+
+    reject() {
+        let arg = arguments
+        let callback = this.failCallBacks.shift()
+        if (this._status === this.STATUS.REJECT && callback) {
+            callback.apply(callback, arg)
+        }
+    }
+
+    then(success, fail) {
+        this.done(success)
+        this.fail(fail)
+        return this
+    }
+
+    done(fun) {
+        if (this._isFunction(fun)) {
+            if (this._status === this.STATUS.RESOLVE) {
+                // 结果
+                fun.apply(fun, this.succArg)
+            } else {
+                this.successCallBacks.push(fun)
+            }
+        }
+        return this
+    }
+
+    fail(fun) {
+        if (this._isFunction(fun)) {
+            if (this._status === this.STATUS.REJECT) {
+                fun.apply(fun, this.failArg) 
+            } else {
+                this.failCallBacks.push(fun) 
+            }
+        }
+        return this 
+    }
 }
 
 const testPromise = () => {
-    let promise1 = new TomPromise(function(resolve, reject) {
-        setTimeout(function() {
-            resolve('foo');
-        }, 300)
-    })
-    promise1.then(function(value) {
-        console.log("3333",value)
+    return new PromiseA((resolve, reject) => {
+        setTimeout(() => {
+            let a = 10
+            resolve(a)
+        }, 2000)
+    }).then((value) => {
+        console.log("测试 promise", value)
     })
 }
 
 const testPromiseAll = () => {
-    let promise1 = new TomPromise(function(resolve, reject) {
+    let p1 = new PromiseA((resolve, reject) => {
         setTimeout(() => {
-            resolve('promise1')
-        }, 0)
+            let a = 10
+            resolve(a)
+        }, 2000)
     })
 
-    let promise2 = new TomPromise(function(resolve, reject) {
+    let p2 = new PromiseA((resolve, reject) => {
         setTimeout(() => {
-            resolve('promise2')
-        }, 300)
+            let a = 3
+            resolve(a)
+        }, 3000)
     })
-
-    let promise3 = new TomPromise(function(resolve, reject) {
-        setTimeout(() => {
-            resolve('promise3')
-        }, 100)
-    })
-
-    TomPromise.all([promise1, promise2, promise3]).then((values) => {
-        console.log("成功的结果是",values)
-    }, (err) => {
-        console.log("错误了", err)
+    return PromiseA.all([p1, p2]).then((value) => {
+        console.log('promiseAll', value)
     })
 }
 
-const __main = () => {
+const testPromoiseRace = () => {
+    let p1 = new PromiseA((resolve, reject) => {
+        setTimeout(() => {
+            let a = 10
+            resolve(a)
+        }, 2000)
+    })
+
+    let p2 = new PromiseA((resolve, reject) => {
+        setTimeout(() => {
+            let a = 3
+            resolve(a)
+        }, 1000)
+    })
+    return PromiseA.race([p1, p2]).then((value) => {
+        console.log('promiseRace', value)
+    })
+}
+
+const main = () => {
     testPromise()
     testPromiseAll()
+    testPromoiseRace()
 }
 
-__main()
+main()
+
